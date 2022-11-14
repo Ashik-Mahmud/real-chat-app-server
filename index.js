962cae6c739232ce9f49cf215325218ac96234a9
ashikmahmud <ashikmamud187@gmail.com>
2022-11-15 03:28:40 +0600

    put error handle message for this

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const app = require('./app');
-require('colors');
+const app = require("./app");
+require("colors");
 
+/* Error Handling */
 
+/* 404 route */
+app.use((err, req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found" + err.message,
+  });
+  next();
+});
+
+/* 500 route */
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  res.json({
+    success: false,
+    message: "Internal Server Error",
+  })
+  error.status = 404;
+  next(error);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
 
 /* start server */
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`.blue.bold);
-    }
-);
+  console.log(`Server started on port ${port}`.blue.bold);
+});
