edd979aed18a7d403bf80374ffc7bcfea2c611e6
ashikmahmud <ashikmamud187@gmail.com>
2022-11-15 03:40:29 +0600

    added jwt token for authGuard

--- middlewares/AuthGuard.js
+++ middlewares/AuthGuard.js
@@ -1,13 +1,17 @@
+const jwt = require("jsonwebtoken");
+
 const AuthGuard = (req, res, next) => {
   const token = req.header("authorization").split(" ")[1];    
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
   }
 };
+
+module.exports = AuthGuard;
