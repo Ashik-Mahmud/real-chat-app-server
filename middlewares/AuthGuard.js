d43d8b623661973840afd0cf922d7563431d735f
ashikmahmud <ashikmamud187@gmail.com>
2022-11-15 03:37:51 +0600

    create middleware for authGuard

---
+++ middlewares/AuthGuard.js
@@ -0,0 +1,13 @@
+const AuthGuard = (req, res, next) => {
+  const token = req.header("authorization").split(" ")[1];    
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
