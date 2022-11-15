fba131936b95cfc0907b5a8f3741fa24c307f2e3
ashikmahmud <ashikmamud187@gmail.com>
2022-11-15 16:26:55 +0600

    finished user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
         type: String,
-        required: true
+        default: "https://www.vippng.com/png/full/416-4161690_empty-profile-picture-blank-avatar-image-circle.png"
     },
     isVerified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    blockedBy: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-});
-
-const Users = mongoose.model('Users', userSchema);
+const Users = mongoose.model("Users", userSchema);
 module.exports = Users;
