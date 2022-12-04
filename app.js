18151f6c8e1b7ca67bab35f09cde113b43e3f02e
ashikmahmud <ashikmamud187@gmail.com>
2022-12-04 18:54:32 +0600

    yah fixe little in cors issues

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const cors = require('cors');
-
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
-
-
 /* apply middle wares */
-app.use(cors());
-app.use(express.json({limit: "50mb"}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 /* imports routes */
-const userRouter = require('./routes/userRoutes');
-const chatRouter = require("./routes/chatRoutes")
-const ConnectedDB = require('./Utils/DatabaseConnection');
-
+const userRouter = require("./routes/userRoutes");
+const chatRouter = require("./routes/chatRoutes");
+const ConnectedDB = require("./Utils/DatabaseConnection");
 
 /* connection */
 ConnectedDB();
 
-
 /* apply routes */
 
 /* user route */
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
 /* chat route */
-app.use("/api/chat", chatRouter)
-
-
-
-
-
+app.use("/api/chat", chatRouter);
 
 module.exports = app;
